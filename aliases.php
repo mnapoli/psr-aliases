33cb54371812dca019db44d086e84e100a8c29db
Matthieu Napoli <matthieu@mnapoli.fr>
2016-09-05 12:58:10 +0200

    First commit

---
+++ aliases.php
@@ -0,0 +1,17 @@
+<?php
+
+// PSR-3
+class_alias('Psr\Log\LoggerInterface', 'Psr\Log\Logger', false);
+
+// PSR-6
+class_alias('Psr\Cache\CacheItemInterface', 'Psr\Cache\CacheItem', false);
+class_alias('Psr\Cache\CacheItemPoolInterface', 'Psr\Cache\CacheItemPool', false);
+
+// PSR-7
+class_alias('Psr\Http\Message\MessageInterface', 'Psr\Http\Message\Message', false);
+class_alias('Psr\Http\Message\RequestInterface', 'Psr\Http\Message\Request', false);
+class_alias('Psr\Http\Message\ResponseInterface', 'Psr\Http\Message\Response', false);
+class_alias('Psr\Http\Message\ServerRequestInterface', 'Psr\Http\Message\ServerRequest', false);
+class_alias('Psr\Http\Message\StreamInterface', 'Psr\Http\Message\Stream', false);
+class_alias('Psr\Http\Message\UploadedFileInterface', 'Psr\Http\Message\UploadedFile', false);
+class_alias('Psr\Http\Message\UriInterface', 'Psr\Http\Message\Uri', false);
